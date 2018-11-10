2895057be07bd4c55a0aaf9c7524c3458a4316c6
Etherless <pabloimrik17@gmail.com>
2018-11-10 03:19:25 +0100

    Added types

--- app/src/main/java/com/example/etherless/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/etherless/dinnerdecider/MainActivity.kt
@@ -4,17 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    val foodList = arrayListOf("Chines", "Hamburger", "Pizza", "Indian")
+    private val foodList: ArrayList<String> = arrayListOf("Chines", "Hamburger", "Pizza", "Indian")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         decideBtn.setOnClickListener {
-            val random = Random();
-            val randomFood = random.nextInt(foodList.count())
+            val random: Random = Random()
+            val randomFood: Int = random.nextInt(foodList.count())
             selectedFoodTxt.text = foodList[randomFood]
         }
 
