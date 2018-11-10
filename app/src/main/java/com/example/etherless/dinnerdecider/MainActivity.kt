6e83f15d72ddf56ce355ee5a688c9dadc06d39d0
Etherless <pabloimrik17@gmail.com>
2018-11-10 02:59:40 +0100

    Added list of food available. Added function on decide button. Added function on addFood button

--- app/src/main/java/com/example/etherless/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/etherless/dinnerdecider/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.etherless.dinnerdecider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val foodList = arrayListOf("Chines", "Hamburger", "Pizza", "Indian")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+        decideBtn.setOnClickListener {
+            val random = Random();
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            if (addFoodTxt.text.isNotEmpty()) {
+                foodList.add(addFoodTxt.text.trim().toString())
+                addFoodTxt.text.clear()
+            }
+        }
     }
 }
